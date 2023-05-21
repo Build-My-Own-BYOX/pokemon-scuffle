e103e5e695b53d84c7ac642e51824ec30bd9894e
Duong Tran <duongtt@cystack.net>
2023-05-21 16:12:02 +0700

    apply props passing to atlas and search view

---
+++ PokeScuffleApp/src/components/molecules/SearchPokemon.tsx
@@ -0,0 +1,141 @@
+import React, { Component } from "react";
+import { View, TextInput, Button, Alert, StyleSheet } from "react-native";
+
+import axios from "axios";
+import pokemonUtils from "pokemon";
+import { Pokemon, PokemonType } from "../../types/Pokemon";
+import {
+  PokemonDetailResponse,
+  PokemonSpecieDetailResponse,
+  PokemonTypeBriefResponse,
+} from "../../types/PokemonAPIResponse";
+
+const POKE_API_BASE_URL = "https://pokeapi.co/api/v2";
+
+interface SearchPokemonProps {
+  pokemonInst: Pokemon;
+  setCallerPokemonInst: (pokemon: Pokemon) => void;
+  setCallerLoadingStatus: (isLoading: boolean) => void;
+}
+
+interface State {
+  searchInput: string;
+}
+
+class SearchPokemon extends Component<SearchPokemonProps, State> {
+  constructor(props: SearchPokemonProps) {
+    super(props);
+
+    this.state = {
+      searchInput: "", // the currently input text
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.headContainer}>
+        <View style={styles.textInputContainer}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(searchInput) => this.setState({ searchInput })}
+            value={this.state.searchInput}
+            placeholder="Search Pokémon"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.searchPokemon} title="Search" color="#0064e1" />
+        </View>
+      </View>
+    );
+  }
+
+  searchPokemon = async () => {
+    try {
+      const pokemonID = pokemonUtils.getId(this.state.searchInput); // check if the entered Pokémon name is valid
+
+      // start loading
+      this.props.setCallerLoadingStatus(true);
+
+      const { data: pokemonDetail } = await axios.get<PokemonDetailResponse>(
+        `${POKE_API_BASE_URL}/pokemon/${pokemonID}`
+      );
+      const { data: pokemonSpecieDetail } =
+        await axios.get<PokemonSpecieDetailResponse>(
+          `${POKE_API_BASE_URL}/pokemon-species/${pokemonID}`
+        );
+
+      // load done
+      this.props.setCallerLoadingStatus(false);
+
+      this.props.setCallerPokemonInst({
+        id: pokemonID,
+        name: pokemonDetail.name,
+        pic: pokemonDetail.sprites.front_default,
+        types: this.mapAPITypesToPokemonTypes(pokemonDetail.types),
+        desc: this.getDescription(pokemonSpecieDetail.flavor_text_entries),
+      });
+    } catch (err) {
+      Alert.alert("Error", "Pokémon not found");
+    }
+  };
+
+  getDescription = (entries) =>
+    entries.find((item) => item.language.name === "en").flavor_text;
+
+  /**
+   * Maps API types to Pokémon type slots.
+   * @param apiTypes - a list of brief pokemon types extracted from API response
+   * @returns An array of Pokémon type slots.
+   */
+  mapAPITypesToPokemonTypes = (
+    apiTypes: PokemonTypeBriefResponse[]
+  ): PokemonType[] => {
+    return apiTypes.map((item) => {
+      if (
+        !item ||
+        typeof item.slot !== "number" ||
+        !item.type ||
+        typeof item.type.name !== "string" ||
+        typeof item.type.url !== "string"
+      ) {
+        return {
+          id: 0,
+          name: "",
+          url: "",
+        };
+      }
+
+      // extract type id from field url
+      const typeId = item.type.url.split("/").slice(-2, -1)[0];
+      return {
+        id: parseInt(typeId) ?? 0,
+        name: item.type.name,
+        url: item.type.url,
+      };
+    });
+  };
+}
+
+const styles = StyleSheet.create({
+  headContainer: {
+    flex: 1,
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  textInputContainer: {
+    flex: 2,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  textInput: {
+    height: 35,
+    marginBottom: 10,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    backgroundColor: "#eaeaea",
+    padding: 5,
+  },
+});
+
+export default SearchPokemon;
--- PokeScuffleApp/src/components/pages/Atlas.tsx
+++ PokeScuffleApp/src/components/pages/Atlas.tsx
@@ -2,35 +2,25 @@ import React, { Component } from "react";
 import {
   SafeAreaView,
   View,
-  Text,
-  TextInput,
-  Button,
-  Alert,
   StyleSheet,
   ActivityIndicator,
 } from "react-native";
 
-import axios from "axios";
-import pokemonUtils from "pokemon";
-import { Pokemon, PokemonType } from "../../types/Pokemon";
+import { Pokemon } from "../../types/Pokemon";
 import PokemonView from "../molecules/PokemonView";
-import { PokemonDetailResponse, PokemonSpecieDetailResponse, PokemonTypeBriefResponse } from "../../types/PokemonAPIResponse";
-
-const POKE_API_BASE_URL = "https://pokeapi.co/api/v2";
+import SearchPokemon from "../molecules/SearchPokemon";
 
 interface State {
   isLoading: boolean;
-  searchInput: string;
   pokemonInst: Pokemon;
 }
 
-export default class Atlas extends Component<{}, State> {
+class Atlas extends Component<{}, State> {
   constructor(props: {}) {
     super(props);
 
     this.state = {
       isLoading: false, // decides whether to show the activity indicator or not
-      searchInput: "", // the currently input text
       pokemonInst: {
         id: 0,
         name: "",
@@ -41,29 +31,25 @@ export default class Atlas extends Component<{}, State> {
     };
   }
 
+  setPokemonInst = (updatedPokemon: Pokemon) => {
+    this.setState({ pokemonInst: updatedPokemon });
+  };
+
+  setLoadingStatus = (isLoading: boolean) => {
+    this.setState({ isLoading: isLoading });
+  };
+
   render() {
-    const { searchInput, isLoading, pokemonInst } = this.state; // extract the Pokémon data from the state
+    const { isLoading, pokemonInst } = this.state; // extract the Pokémon data from the state
 
     return (
       <SafeAreaView style={styles.wrapper}>
         <View style={styles.container}>
-          <View style={styles.headContainer}>
-            <View style={styles.textInputContainer}>
-              <TextInput
-                style={styles.textInput}
-                onChangeText={(searchInput) => this.setState({ searchInput })}
-                value={this.state.searchInput}
-                placeholder="Search Pokémon"
-              />
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button
-                onPress={this.searchPokemon}
-                title="Search"
-                color="#0064e1"
-              />
-            </View>
-          </View>
+          <SearchPokemon
+            pokemonInst={pokemonInst}
+            setCallerPokemonInst={this.setPokemonInst}
+            setCallerLoadingStatus={this.setLoadingStatus}
+          />
 
           <View style={styles.mainContainer}>
             {isLoading && <ActivityIndicator size="large" color="#0064e1" />}
@@ -74,68 +60,6 @@ export default class Atlas extends Component<{}, State> {
       </SafeAreaView>
     );
   }
-
-  searchPokemon = async () => {
-    try {
-      const pokemonID = pokemonUtils.getId(this.state.searchInput); // check if the entered Pokémon name is valid
-      this.setState({
-        isLoading: true, // show the loader while request is being performed
-      });
-      const { data: pokemonDetail} = await axios.get<PokemonDetailResponse>(
-        `${POKE_API_BASE_URL}/pokemon/${pokemonID}`
-      );
-      const { data: pokemonSpecieDetail } = await axios.get<PokemonSpecieDetailResponse>(
-        `${POKE_API_BASE_URL}/pokemon-species/${pokemonID}`
-      );
-      this.setState((prevState) => ({
-        pokemonInst: {
-          ...prevState.pokemonInst, // Keep the existing attributes
-          id: pokemonID,
-          name: pokemonDetail.name,
-          pic: pokemonDetail.sprites.front_default,
-          types: this.mapAPITypesToPokemonTypes(pokemonDetail.types),
-          desc: this.getDescription(pokemonSpecieDetail.flavor_text_entries),
-        },
-        isLoading: false, // hide loader
-      }));
-    } catch (err) {
-      Alert.alert("Error", "Pokémon not found");
-    }
-  };
-
-  getDescription = (entries) =>
-    entries.find((item) => item.language.name === "en").flavor_text;
-
-  /**
-   * Maps API types to Pokémon type slots.
-   * @param apiTypes - a list of brief pokemon types extracted from API response 
-   * @returns An array of Pokémon type slots.
-  */
-  mapAPITypesToPokemonTypes = (apiTypes: PokemonTypeBriefResponse[]): PokemonType[] => {
-    return apiTypes.map((item) => {
-      if (
-        !item ||
-        typeof item.slot !== "number" ||
-        !item.type ||
-        typeof item.type.name !== "string" ||
-        typeof item.type.url !== "string"
-      ) {
-        return {
-          id: 0,
-          name: "",
-          url: "",
-        };
-      }
-
-      // extract type id from field url
-      const typeId = item.type.url.split("/").slice(-2, -1)[0];
-      return {
-        id: parseInt(typeId) ?? 0,
-        name: item.type.name,
-        url: item.type.url,
-      };
-    });
-  };
 }
 
 const styles = StyleSheet.create({
@@ -147,26 +71,9 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: "#F5FCFF",
   },
-  headContainer: {
-    flex: 1,
-    flexDirection: "row",
-    marginTop: 100,
-  },
-  textInputContainer: {
-    flex: 2,
-  },
-  buttonContainer: {
-    flex: 1,
-  },
   mainContainer: {
     flex: 9,
   },
-  textInput: {
-    height: 35,
-    marginBottom: 10,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    backgroundColor: "#eaeaea",
-    padding: 5,
-  },
 });
+
+export default Atlas;
